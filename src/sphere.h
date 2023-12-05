26bfd4ad120a8df47c1c3750ac9a1e1a71a46eb8
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-12-05 20:10:37 +0100

    added interval class

--- src/sphere.h
+++ src/sphere.h
@@ -7,7 +7,7 @@ class sphere : public hittable{
 public:
     sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = r.origin() - center;
         double a = r.direction().length_squared();
         double half_b = dot(oc, r.direction());
@@ -20,9 +20,9 @@ public:
 
         double sqrtd = sqrt(discriminant);
         double root = (-half_b - sqrtd) / a;
-        if(root <= ray_tmin || root >= ray_tmax){
+        if(!ray_t.surrounds(root)){
             root = (-half_b + sqrtd) / a;
-            if(root <= ray_tmin || root >= ray_tmax)
+            if(!ray_t.surrounds(root))
                 return false;
         }
 
