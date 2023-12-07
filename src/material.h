437858308073896d2151e4c4034d9ba58123e407
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-12-07 04:15:56 +0100

    Schlicks reflectance

--- src/material.h
+++ src/material.h
@@ -63,7 +63,7 @@ public:
         bool cannot_refract = refraction_ratio * sin_theta > 1.0;
         vec3 direction;
 
-        if(cannot_refract){
+        if(cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()){
             direction = reflect(unit_direction, rec.normal);
         }else{
             direction = refract(unit_direction, rec.normal, refraction_ratio);
@@ -74,6 +74,13 @@ public:
     }
 private:
     double ir;
+
+    static double reflectance(double cosine, double ref_index){
+        // Schlick's approximation for reflectance.
+        double r0 = (1 - ref_index) / (1 + ref_index);
+        r0 *= r0;
+        return r0 + (1- r0) * pow(1 - cosine, 5);
+    }
 };
 
 #endif
