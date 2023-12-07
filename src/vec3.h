56abe98d48f9e9f8065996e47fc3cc8d0ac4580b
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-12-07 04:10:30 +0100

    dielectrics and refraction

--- src/vec3.h
+++ src/vec3.h
@@ -144,4 +144,12 @@ inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
 
+// v and n must be unit vectors
+inline vec3 refract(const vec3& v, const vec3 n, double etai_over_etat){
+    double cos_theta = fmin(dot(-v, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (v + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_parallel + r_out_perp;
+}
+
 #endif
