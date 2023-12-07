d03824c38987e31d3e0d50ca9707656137dbe33d
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-12-07 23:54:22 +0100

    bvh construction done!

--- src/blines_general.h
+++ src/blines_general.h
@@ -38,6 +38,10 @@ inline double random_double(double min, double max){
     return min + (max - min) * random_double();
 }
 
+inline int random_int(int min, int max){
+    return static_cast<int>(random_double(min, max + 1));
+}
+
 #include "ray.h"
 #include "vec3.h"
 #include "interval.h"
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "bvh.h"
 
 #include <iostream>
 #include <fstream>
@@ -53,6 +54,8 @@ void fun_balls(){
 
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    world = hittable_list(make_shared<bvh_node>(world));
 
     camera cam("image2.ppm");
     cam.aspect_ratio = 16.0 / 9.0;
