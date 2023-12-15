98277811f8624bf25a0f3bb102a794d40338c91c
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-12-15 01:37:25 +0100

    pdf.h

---
+++ src/pdf.h
@@ -0,0 +1,67 @@
+#ifndef PDF_H
+#define PDF_H
+
+#include "blines.h"
+#include "onb.h"
+#include "hittable.h"
+
+class pdf {
+public:
+    virtual ~pdf() {}
+    
+    virtual double value(const vec3& direction) const = 0;
+    virtual vec3 generate() const = 0;
+};
+
+class sphere_pdf : public pdf {
+public:
+    sphere_pdf() {}
+
+    double value(const vec3& direction) const override {
+        return 1 / (4 * pi);
+    }
+
+    vec3 generate() const override {
+        return random_unit_vector();
+    }
+};
+
+class cosine_pdf : public pdf {
+public:
+    // w is the surface normal
+    cosine_pdf(const vec3& w) {
+        uvw.build_from_w(w);
+    }
+
+    double value(const vec3& direction) const override {
+        double cosine_theta = dot(unit_vector(direction), uvw.w());
+        return fmax(0, cosine_theta / pi);
+    }
+
+    vec3 generate() const override {
+        return uvw.local(random_cosine_direction());
+    }
+
+private:
+    onb uvw;
+};
+
+class hittable_pdf : public pdf {
+public:
+    hittable_pdf(const hittable& _objects, const point3& _origin)
+        : objects(_objects), origin(_origin) {}
+
+    double value(const vec3& direction) const override {
+        return objects.pdf_value(origin, direction);
+    }
+
+    vec3 generate() const override {
+        return objects.random(origin);
+    }
+
+private:
+    const hittable& objects;
+    point3 origin;
+};
+
+#endif
